5d45aa48131d7bf4775798c835fc1d301a7bf136
chris4824 <chris482422@gmail.com>
2016-01-31 09:49:34 -0500

    use new buildscript

---
+++ build.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Parameter 2: Build mode: restart | new | wipe | [leave it empty]
+# restart = clean build
+# new = tag as a released build
+# wipe = clean build and tag as a released build
+# . build.sh jfltetmo restart
+
+# Colorize and add text parameters
+grn=$(tput setaf 2)             #  green
+txtbld=$(tput bold)             # Bold
+bldgrn=${txtbld}$(tput setaf 2) #  green
+bldblu=${txtbld}$(tput setaf 4) #  blue
+txtrst=$(tput sgr0)             # Reset
+
+DEVICE="$1"
+MODE="$2"
+
+# Time of build startup
+res1=$(date +%s.%N)
+
+# Reading mode
+if [ ! -z $MODE ]; then
+if [ $MODE == "restart" ]; then
+       echo -e "${grn}Local manifest synced ${txtrst}"
+       echo -e "${grn}Restarting build without cleaning ${txtrst}"
+       echo -e "${bldblu}Cleaning up out folder and setting CCACHE ${txtrst}"
+       export CCACHE_DIR=/media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/.ccache
+       echo -e "${bldblu}CCACHE is set to /media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/.ccache${txtrst}"
+       echo -e "${bldblu}CCACHE current status${txtrst}"
+       /media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/ccache -s
+       read -p "Press [Enter] key to proceed..."
+       echo -e "${bldblu}Setting up build environment ${txtrst}"
+       . build/envsetup.sh
+       echo -e "${bldblu}Lunching device... ${txtrst}"
+       lunch "broken_$DEVICE-userdebug"
+       echo -e "${grn}Device and vendor repos synced ${txtrst}"
+       mka tesla
+       res2=$(date +%s.%N)
+       echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
+fi
+else
+       echo -e "${bldblu}Please set a build flag${txtrst}"
+fi
+if [ ! -z $MODE ]; then
+if [ $MODE == "new" ]; then
+       echo -e "${grn}Local manifest synced ${txtrst}"
+       echo -e "${grn}New build cleaning out directory ${txtrst}"
+       echo -e "${bldblu}Cleaning up out folder and setting CCACHE ${txtrst}"
+       export CCACHE_DIR=/media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/.ccache
+       echo -e "${bldblu}CCACHE is set to /media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/.ccache${txtrst}"
+       echo -e "${bldblu}CCACHE current status${txtrst}"
+       /media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/ccache -s
+       read -p "Press [Enter] key to proceed..."
+       make clobber
+       echo -e "${bldblu}Setting up build environment ${txtrst}"
+       . build/envsetup.sh
+       echo -e "${bldblu}Lunching device... ${txtrst}"
+       lunch "broken_$DEVICE-userdebug"
+       echo -e "${grn}Device and vendor repos synced ${txtrst}"
+       mka tesla
+       res2=$(date +%s.%N)
+       echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
+fi
+else
+       echo -e "${bldblu}Please set a build flag${txtrst}"
+fi
+if [ ! -z $MODE ]; then
+if [ $MODE == "wipe" ]; then
+       echo -e "${grn}Local manifest synced ${txtrst}"
+       echo -e "${grn}New build cleaning out directory and clearing CCACHE ${txtrst}"
+       echo -e "${bldblu}Cleaning up out folder and setting CCACHE ${txtrst}"
+       export CCACHE_DIR=/media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/.ccache
+       echo -e "${bldblu}CCACHE is set to /media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/.ccache${txtrst}"
+       echo -e "${bldblu}CCACHE current status${txtrst}"
+       /media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/ccache -s
+       read -p "Press [Enter] key to proceed..."
+       make clobber
+       /media/chris/Drive/tesla/prebuilts/misc/linux-x86/ccache/ccache -C
+       read -p "Press [Enter] key to proceed..."
+       echo -e "${bldblu}Setting up build environment ${txtrst}"
+       . build/envsetup.sh
+       echo -e "${bldblu}Lunching device... ${txtrst}"
+       lunch "tesla_$DEVICE-userdebug"
+       echo -e "${grn}Device and vendor repos synced ${txtrst}"
+       mka tesla
+       res2=$(date +%s.%N)
+       echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
+fi
+else
+       echo -e "${bldblu}Please set a build flag${txtrst}"
+fi
